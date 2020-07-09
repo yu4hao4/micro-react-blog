f2a3bfad2ed59cd46b11387abb0c8da96901e4e5
e <1792314541@qq.com>
2020-07-09 19:35:12 +0800

    init

---
+++ micro-react-gateway/src/main/java/yuhao/GatewayApplication.java
@@ -0,0 +1,19 @@
+package yuhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yuhao5
+ * @date 2020-07-09
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
