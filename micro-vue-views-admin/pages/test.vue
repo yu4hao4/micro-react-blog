8066e33b063a8431c0fd1e43b4e9a22a3a119be4
e <1792314541@qq.com>
2020-07-16 21:34:53 +0800

    2020-7-6 21:35 喻浩
    前端添加 markdown 渲染插件

---
+++ micro-vue-views-admin/pages/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-html="getArticle">
+  </div>
+</template>
+
+<script>
+  import { mavonEditor } from 'mavon-editor'
+  import marked from 'marked'
+  export default {
+    name: "test",
+    computed:{
+      getArticle(){
+        // let render = mavonEditor.getMarkdownIt();
+        console.log(this.content)
+        return marked(this.content);
+      }
+    },
+    data(){
+      return{
+        content:"# how to use mavonEditor in nuxt.js\n" +
+          "# 1232131231\n" +
+          "<h4><a id=\"how_to_use_mavonEditor_in_nuxtjs_0\"></a>how to use mavonEditor in nuxt.js</h4> <h1><a id=\"1232131231_1\"></a>1232131231</h1>\n"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
