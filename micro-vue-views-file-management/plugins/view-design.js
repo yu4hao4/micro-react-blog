da6b3ce49bb460031d3a2aa389b0c9838cc65316
e <1792314541@qq.com>
2020-07-24 16:54:42 +0800

    2020-7-24 16:55 喻浩
    添加图片管理前端项目

---
+++ micro-vue-views-file-management/plugins/view-design.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+import locale from 'view-design/dist/locale/zh-CN';
+
+Vue.use(ViewUI, {
+  locale
+});
